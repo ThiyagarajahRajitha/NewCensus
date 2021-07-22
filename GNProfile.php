0d56d5fdef9c1833dc408838de0ce5f3d7141659
dasunienirmani <samarasekarakpdn@gmail.com>
2021-07-22 15:33:27 +0530

    changed1

--- GNProfile.php
+++ GNProfile.php
@@ -16,6 +16,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src='https://kit.fontawesome.com/a076d05399.js'>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    
+	
+
+
+
+
     <style type="text/css">
         .right {
             float: right;
@@ -44,13 +50,12 @@
             margin-left:260px;
         }
     </style>
-    </head>
 
     <?php
     include 'config.php';
-    session_start();
 
-    $user=$_SESSION['userid'];
+session_start();
+$user=$_SESSION['userid'];
 $sql="SELECT GramaNiladhariId FROM user WHERE userId='$user'";
 if($result=mysqli_query($link,$sql)){
     if(mysqli_num_rows($result) >0){
@@ -59,7 +64,6 @@ if($result=mysqli_query($link,$sql)){
         $_SESSION['$gnId']=$gnId;
     }
 }
-
 
 $username=$_SESSION['username'];
 $sql="SELECT  divisionalSecretariatId FROM gramaniladhari WHERE gramaNiladhariId='$gnId'";
@@ -73,7 +77,6 @@ if($result=mysqli_query($link,$sql)){
 
     }
 }
-
 $sql="SELECT divisionalSecretariatName FROM divisionalsecretariat WHERE divisionalSecretariatId='$divSecId' ";
 if($result=mysqli_query($link,$sql)){
 if(mysqli_num_rows($result) >0){
@@ -84,7 +87,6 @@ if(mysqli_num_rows($result) >0){
 
 }
 }
-
 
 $sql="SELECT districtId FROM divisionalsecretariat WHERE divisionalSecretariatId='$divSecId' ";
 if($result=mysqli_query($link,$sql)){
@@ -108,6 +110,8 @@ if(mysqli_num_rows($result) >0){
 }
 }
 
+
+
 $sql="SELECT provinceId FROM district WHERE districtId='$distId' ";
 if($result=mysqli_query($link,$sql)){
 if(mysqli_num_rows($result) >0){
@@ -118,7 +122,6 @@ if(mysqli_num_rows($result) >0){
 
 }
 }
-
 
 $sql="SELECT provinceName FROM province WHERE provinceId='$provId' ";
 if($result=mysqli_query($link,$sql)){
@@ -183,52 +186,104 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 }
-
-$sql="SELECT * FROM gramaniladhariofficer WHERE gramaNiladhariId='$gnId'";
-$result=mysqli_query($link,$sql) or die (mysqli_error());
-global $gnFName;
-while ($row=mysqli_fetch_array($result)){
-    if(mysqli_num_rows($result) >0){
-        $row=mysqli_fetch_assoc($result);
-        $gnFName=$row['gramaniladhariOfficerFirstname'];
-        $gnLName=$row['gramaNiladhariOfficerLastName'];
-        $genderid=$row['genderid'];
-        $contactNo=$row['contactNo'];
-        $gnId=$row['gramaNiladhariId'];
-        $officialAddress=$row['Officialaddress'];
-        $email=$row['email'];
-
-
-        $_SESSION['gnFName']=$gnFName;
-        $_SESSION['gnLName']=$gnLName;
-        $_SESSION['contactNo']=$contactNo;
-        $_SESSION['gnId']=$gnId;
-        $_SESSION['officialAddress']=$officialAddress;
-        $_SESSION['email']=$email;
-
-    }else{
-        $gnFName="";
-        $gnLName="";
-        $genderid="";
-        $contactNo="";
-        $gnId="";
-        $officialAddress="";
-        $email="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['Edit'])){
+        $query="UPDATE gramaniladhariofficer SET gramaniladhariOfficerid='$gnOfficerId',gramaniladhariOfficerFirstname='$gnFName',gramaNiladhariOfficerLastName='$gnLName',genderid='$gender',contactNo='$contactNo',Officialaddress='$address' WHERE gramaNiladhariId='$gnId'";
+        if (mysqli_query($link,$sql)){
+            echo "GN profile updated successfully!";
+            header("location:GNProfile.php");
+        }else{
+            echo "Error gn: " . $sql . "<br>";
+        }
+    
+    
     }
-
 }
 
+    $sql="SELECT * FROM gramaniladhariofficer WHERE gramaNiladhariId='$gnId'";
+    $result=mysqli_query($link,$sql) or die (mysqli_error());
+    global $gnFName;
+    while ($row=mysqli_fetch_array($result)){
+        if(mysqli_num_rows($result) >0){
+			$row=mysqli_fetch_assoc($result);
+			$gnFName=$row['gramaniladhariOfficerFirstname'];
+            $gnLName=$row['gramaNiladhariOfficerLastName'];
+            $genderid=$row['genderid'];
+            $contactNo=$row['contactNo'];
+            $gnId=$row['gramaNiladhariId'];
+            $officialAddress=$row['Officialaddress'];
+            $email=$row['email'];
+
+
+            $_SESSION['gnFName']=$gnFName;
+            $_SESSION['gnLName']=$gnLName;
+            $_SESSION['contactNo']=$contactNo;
+            $_SESSION['gnId']=$gnId;
+            $_SESSION['officialAddress']=$officialAddress;
+            $_SESSION['email']=$email;
+
+        }else{
+            $gnFName="";
+            $gnLName="";
+            $genderid="";
+            $contactNo="";
+            $gnId="";
+            $officialAddress="";
+            $email="";
+        }
+    }
+            
+
+    
+    
+    ?>
+
+    
 
 
 
 
+</head>
 
-?>
+<body style="background-color: rgb(179, 226, 185);">
 
+    <div class="container-fluid">
+        <section>
+            <div class="row">
+                <div class="col-sm-9"
+                    style="background-color: rgb(95, 143, 103);font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                    <h1>E-Census Sri Lanka</h1>
+                </div>
+                <div class="col-sm-1" style="background-color: rgb(95, 143, 103);">
+                    <i class="fa fa-bell" style="font-size:36px; margin-top:24px; float:right;"></i>
 
-<body>
+                </div>
 
-    <form action="" method="post" form id="submittedForm"  style="display: none;">
+                <div class="col-sm-1" style="background-color: rgb(95, 143, 103);">
+                    <label style="margin-top:30px; float:right;"><h5> <?php $userId=$_SESSION['userid'];
+                        $username=$_SESSION['username']; echo $username ?></h5>
+                        
+                        
+                    </label>
+                </div>
+
+                <div class="col-sm-1" style="background-color: rgb(95, 143, 103);">
+                    <i class='far fa-user-circle' style='font-size:36px; float: right;margin-top:30px;'></i>
+                    <!--<img src="images/profile.png" style="float: right; width: 50px; height: 50px;margin-top: 20px;">-->
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-1"></div>
+
+                <div class="col-lg-10">
+                    <div id="bg">
+                        <h2 style="text-align: center; margin-top:10px;">GN Profile</h2>
+
+                        <hr />
+
+                        <form action="" method="post" form id="newForm">
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
                                 <input type="text" class="form-control input-lg" name="name"
                                     placeholder="<?php $username=$_SESSION['username']; echo $username ?> " disabled required>
@@ -253,12 +308,12 @@ while ($row=mysqli_fetch_array($result)){
                             <br />
 
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
-                                <input type="text" class="form-control input-lg" name="GNOfficerId"   placeholder="GN Officer Id" value="<?php echo $row ['gramaNiladhariOfficerid']; ?>"
+                                <input type="text" class="form-control input-lg" name="GNOfficerId"   placeholder="GN Officer Id" value="<?php echo $gnId ?>">
                                    >
                             </div>
                             <br/>
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
-                                <input type="text" class="form-control input-lg" name="GnFName"  placeholder="GN First Name" value="<?php echo $gnFName; ?>"
+                                <input type="text" class="form-control input-lg" name="GnFName"  placeholder="GN First Name" value="<?php if(isset($_SESSION['gnFName'])){ echo "gnf"; }?>"
                                     >
                             </div>
                             <br/>
@@ -276,18 +331,19 @@ while ($row=mysqli_fetch_array($result)){
 
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
                                 <input type="text" class="form-control input-lg" name="ContactNo"
-                                    placeholder="Contact Number" value="<?php echo $row['contactNo']; ?>">
+                                    placeholder="Contact Number" value="<?php echo $contactNo ?>" >
                             </div>
                             <br />
 
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
-                                <input type="text" class="form-control input-lg" name="email" value="<?php echo $email ?>">
+                                <input type="text" class="form-control input-lg" name="email" 
+                                    placeholder="email" value="<?php echo $email  ?>">
                             </div>
                             <br/>
                             
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6 ">
 
-                                <textarea rows="5" columns="300" name="officialAddress"  placeholder="Official Address"><?php echo $officialAddress ?></textarea>
+                                <input type="text" class="form-control input-lg" name="officialAddress"  placeholder="Official Address" value="<?php echo $officialAddress ?>">
                             </div>
                             <br />
                             <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
@@ -295,27 +351,22 @@ while ($row=mysqli_fetch_array($result)){
                                 
                             </div>
                             <br/>
+                            <div class="col-5m-12 col-md-6 col-lg-6 col-md-6 col-md-6">
                             
-                            <button type="submit" class="btn btn-success col-sm-3" name="Edit" id="btnEdit" >Edit</button>
-                                
+                            <button type="submit" class="btn btn-success col-md-6" name="Edit" id="btnEdit">Edit</button>
+                                </div>
                          
                             <br/>
                            
                             
                         </form>
-
-
-
-
-
-
-
+                    </div>
+                </div>
 
                 <div class="col-lg-1"></div>
 
                 
 
-            
+            </div>
 </body>
 </html>
-    
